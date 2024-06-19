7d37948023ff45c2ebcd737111f352555ea1900c
Carl Louis C. Labayne <carl.labayne@safc.com.ph>
2024-06-19 14:08:59 +0800

    updated spouse and clients

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -110,6 +110,7 @@ class ClientController {
   async createClient(req: Request, res: Response): Promise<void> {
     try {
       const {
+        address,
         lastname,
         firstname,
         middlename,
@@ -184,8 +185,10 @@ class ClientController {
         id_no,
         assetremarks, 
         b_expiry,
-        citymuncode,
-        id_expiration
+        id_expiration,
+        socialmedia_account,
+        socialmedia_type,
+        area
 
       } = req.body;
   
@@ -215,6 +218,8 @@ class ClientController {
             gender,
             mobile,
             telephone,
+            address,
+            area,
             civilstatus,
             religion,
             email,
@@ -236,10 +241,10 @@ class ClientController {
                 s_middlename: s_middlename ?? '',
                 s_suffix : s_suffix ?? '',
                 s_birthdate: s_birthdate ?? new Date,
-                gender: s_gender ?? '',
-                address: s_address ?? '',
-                mobile: s_mobile ?? '',
-                telephone: s_telephone ?? '',
+                s_gender: s_gender ?? '',
+                s_address: s_address ?? '',
+                s_mobile: s_mobile ?? '',
+                s_telephone: s_telephone ?? '',
                 crm_spouseEducation: {
                   create: {
                     s_educLevel: s_educLevel ?? '',
@@ -261,11 +266,12 @@ class ClientController {
                 createddatetime: new Date(),
               },
             },
-            // crm_address_citymunicipality: {
-            //   connect: {
-            //     citymuncode: citymuncode ?? '',
-            //   }
-            // }
+           crm_clientSocials: {
+            create: {
+              socialmedia_account,
+              socialmedia_type,
+            }
+           }
           },
         }),
         prisma.crm_loan_hdr.create({
@@ -371,209 +377,7 @@ class ClientController {
       }
     }
   }
-  async updatebyProfile(req: Request, res: Response): Promise<void> {
-    const { profile, loanprofile } = req.params;
-    try {
-      const {
-        lastname,
-        firstname,
-        middlename,
-        suffix,
-        birthday,
-        age,
-        gender,
-        mobile,
-        telephone,
-        area,
-        civilstatus,
-        religion,
-        mothersname,
-        email,
-        updatedby,
-        assetremarks,
-        educ_level,
-        totalfair_marketvalue,
-        educ_school,
-        course,
-        id_type,
-        id_no,
-        b_expiry,
-        id_expiration,
-        s_lastname,
-        verified,
-        s_firstname,
-        s_middlename,
-        s_suffix,
-        s_birthdate,
-        s_gender,
-        s_address,
-        s_mobile,
-        s_telephone,
-        loantype,
-        terms,
-        amountapplied,
-        modeofpayment,
-        productid,
-        agentid,
-        business_name,
-        business_nature,
-        business_address,
-        business_contact,
-        net_income
 
-
-
-
-
-
-     
-      } = req.body;
-
-      let existingClient = await prisma.crm_client.findUnique({
-        where: { profile: profile },
-      });
-
-      if (!existingClient) {
-        res.status(404).json({ message: "Existing client not found" });
-      }
-
-      let existingProduct = await prisma.crm_products.findUnique({
-        where: {productid: productid}
-      })
-      if (!existingProduct) {
-        res.status(404).json({ message: "Existing product not found" });
-
-      }
-      const updateClient = await prisma.crm_client.update({
-        where: { profile: profile },
-        data: {
-          lastname,
-          firstname,
-          middlename,
-          suffix,
-          birthday,
-          age,
-          gender,
-          mobile,
-          telephone,
-          area,
-          civilstatus,
-          religion,
-          email,
-          mothersname,
-          updatedby: updatedby ?? "",
-          updateddatetime: new Date(),
-          crm_assets: {
-            updateMany: {
-              where: { profile: profile },
-              data: {
-                assetremarks: assetremarks,
-                totalfair_marketvalue: totalfair_marketvalue
-              },
-            },
-          },
-          crm_clientEducation: {
-            updateMany: {
-              where: { profile: profile },
-              data: {
-                educ_level : educ_level,
-                educ_school: educ_school,
-                course: course,
-              },
-            },
-          },
-          crm_clientId: {
-            updateMany: {
-              where: { profile: profile },
-              data: {
-                id_type: id_type,
-                id_no: id_no,
-                b_expiry: b_expiry,
-                id_expiration: id_expiration,
-                verified: verified,
-                updatedby: updatedby ?? "",
-                updateddatetime: new Date(),
-              },
-            },
-          },
-          // crm_soiBusiness: {
-          //   updateMany: {
-          //     where: { profile: profile },
-          //     data: {},
-          //   },
-          // },
-          // crm_soiOccupation: {
-          //   updateMany: {
-          //     where: { profile: profile },
-          //     data: {},
-          //   },
-          // },
-          // crm_soiOfw: {
-          //   updateMany: {
-          //     where: { profile: profile },
-          //     data: {},
-          //   },
-          // },
-          crm_spouse: {
-            updateMany: {
-              where: { profile: profile },
-              data: {
-                s_lastname: s_lastname,
-                s_firstname: s_firstname,
-                s_middlename: s_middlename,
-                s_suffix: s_suffix,
-                s_birthdate: s_birthdate,
-                gender: s_gender,
-                address: s_address,
-                mobile: s_mobile,
-                telephone: s_telephone,
-
-              },
-            },
-          },
-        },
-      });
-
-  await  prisma.crm_loan_hdr.update({
-        where: { loanprofile: loanprofile },
-        data: {
-          loantype: loantype,
-          terms: terms,
-          modeofpayment: modeofpayment,
-          amountapplied:amountapplied ,
-          agentid: agentid,
-          crm_soiBusiness: {
-            updateMany: {
-              where: { profile: profile },
-              data: {
-                business_name: business_name,
-                business_nature: business_nature,
-                business_address: business_address,
-                business_contact: business_contact,
-                net_income: net_income,
-              },
-            },
-          }
-        }
-      })
-
-      res.status(200).json({
-        message: "Client updated successfully",
-        updateClient,
-      });
-
-      console.log("Update data successful", updateClient);
-    } catch (err) {
-      console.error("Error updating client:", err);
-      if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        // Handle known Prisma errors
-        res.status(400).json({ message: "Bad Request", error: err.message });
-      } else {
-        // Handle other errors
-        res.status(500).json({ message: "Internal Server Error" });
-      }
-    }
-  }
 }
 
 export default new ClientController();
