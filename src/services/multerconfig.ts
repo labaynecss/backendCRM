24946da59e08642601bc336bc8c69d3ce98b7490
Carl Louis C. Labayne <carl.labayne@safc.com.ph>
2024-07-04 13:31:49 +0800

    update files structure

--- src/services/multerconfig.ts
+++ src/services/multerconfig.ts
@@ -4,7 +4,7 @@ import path from 'path';
 // Configure storage for multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); 
+    cb(null, 'src/uploads/'); 
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`); 
