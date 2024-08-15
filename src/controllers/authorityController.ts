762863b247b20ff86a4e8fc6b3a843ad4cf8183c
jamesmiayo <it_jamesmiayo25@yahoo.com>
2024-08-15 16:28:55 +0800

    backend for authority

--- src/controllers/authorityController.ts
+++ src/controllers/authorityController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
+import { count } from 'console'
 
 const prisma = new PrismaClient()
 
@@ -13,24 +14,77 @@ class AuthorityController {
           unsecuredLoan: true,
           collateralLoan: true,
         },
-        // include: {
-        //   crm_users: {
-        //     // Fetching related `crm_users` data
-        //     select: {
-        //       empId: true,
-        //       u_firstname: true,
-        //       u_lastname: true,
-        //       u_middlename: true,
-        //     },
-        //   },
-        // },
       })
 
-      console.log('Fetch success', lists)
+      // Count the total number of authorities
+      const countApprover = await prisma.crm_creditAuthority.count()
+      // console.log('Fetch success', lists)
 
-      res.status(200).json(lists)
+      res.status(200).json({ data: lists, total: [{ countApprover }] })
     } catch (err) {
       console.error('Error retrieving score:', err)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+  public async createAuthority(req: Request, res: Response): Promise<void> {
+    try {
+      const { empId, unsecuredLoan, collateralLoan } = req.body
+      const existingAuthority = await prisma.crm_creditAuthority.findFirst({
+        where: {
+          OR: [{ empId }],
+        },
+      })
+
+      if (existingAuthority) {
+        res.status(400).json({ error: ' already exists.' })
+        return
+      }
+
+      const registerAuthority = await prisma.crm_creditAuthority.create({
+        data: {
+          empId: empId,
+          unsecuredLoan: unsecuredLoan,
+          collateralLoan: collateralLoan,
+        },
+      })
+
+      res.status(201).json({
+        message: 'Created Authority Successfully',
+        registerAuthority,
+      })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  }
+
+  public async updateAuthority(req: Request, res: Response): Promise<void> {
+    try {
+      const { empId } = req.params
+      const { unsecuredLoan, collateralLoan } = req.body
+
+      let existingAuthority = await prisma.crm_creditAuthority.findFirst({
+        where: { empId: empId },
+      })
+
+      if (!existingAuthority) {
+        res.status(404).json({ error: 'Authority not found' })
+        return
+      }
+
+      const user = await prisma.crm_creditAuthority.update({
+        where: {
+          empId: empId,
+        },
+        data: {
+          unsecuredLoan: unsecuredLoan,
+          collateralLoan: collateralLoan,
+        },
+      })
+
+      res.status(200).json({ message: 'User Update Successful', user })
+    } catch (err) {
+      console.error('Error updating user:', err)
       res.status(500).json({ error: 'Internal Server Error' })
     }
   }
--- src/routes/authorityRoutes.ts
+++ src/routes/authorityRoutes.ts
@@ -7,5 +7,6 @@ const router = Router()
 
 router.get('/credit-authority', AuthorityController.authorityList)
 // router.get('/totalscore/:loanprofile', ScoreController.scoreTotal)
-
+router.post('/credit-authority/register', AuthorityController.createAuthority)
+router.put('/credit-authority/:empId', AuthorityController.updateAuthority)
 export const authorityRoutes = router
